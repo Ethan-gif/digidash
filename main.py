eac98de1201b178ba16f9bacd036b8f5f7b75148
Cristo-gif <ethan.johnson3005@gmail.com>
2022-02-27 08:25:15 +1000

    asdfa

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from digi import create_app, db
 if __name__ == "__main__":
     napp = create_app()
     db.create_all(app=create_app())
-    napp.run(debug=True, host='10.0.0.63')
+    napp.run(debug=False, host='0.0.0.0')
